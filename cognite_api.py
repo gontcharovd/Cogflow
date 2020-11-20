18ceba91ec26ef3c79390743dfc4f2e9c01315a4
Denis Gontcharov <denis@gontcharov.dev>
2020-11-20 15:58:24 +0100

    configured fetch_data function

--- cognite_api.py
+++ cognite_api.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import pandas as pd
 
 from dotenv import load_dotenv
 
@@ -8,7 +9,7 @@ PROJECT = 'publicdata'
 RESOURCE = 'timeseries'
 
 
-def get_session():
+def _get_session():
     """Builds a requests Session for the Movielens API."""
     session = requests.Session()
     load_dotenv()
@@ -21,34 +22,59 @@ def get_session():
     return session, base_url
 
 
-def get_request():
-    session, base_url = get_session()
-    url = f'{base_url}'
-    response = session.get(url)
-    response.raise_for_status()
-    return response.json()
-
-
-def post_request(body):
-    session, base_url = get_session()
-    url = f'{base_url}/byids'
-    # data = {'id': "2251496734604682"}
+def _get_data(session, url, body):
     response = session.post(url, json=body)
     response.raise_for_status()
     return response.json()
 
 
-def create_body(ts_ids):
+def _create_body(
+        ids,
+        start_date,
+        end_date,
+        aggregates=['average', 'min'],
+        granularity='5m',
+        limit=10,
+        **kwargs
+):
     body = {
-        "items": [],
-        "ignoreUnknownIds": False
+        'items': [],
+        'start': start_date,
+        'end': end_date,
+        'limit': int(limit),
+        'aggregates': aggregates,
+        'granularity': granularity,
+        'includeOutsidePoints': False,
+        'ignoreUnknownIds': False
     }
-    for id in ts_ids:
+    for id in ids:
         body['items'].append({'id': str(id)})
     return body
 
 
+def fetch_data(ids, start_date, end_date, **kwargs):
+    session, base_url = _get_session()
+    body = _create_body(ids, start_date, end_date, **kwargs)
+    response = _get_data(
+        session=session,
+        url=base_url + '/data/list',
+        body=body
+    )
+    all_df = pd.DataFrame()
+    for data in response.get('items'):
+        df = pd.DataFrame.from_dict(data.get('datapoints'))
+        df.insert(1, 'id', data.get('id'))
+        all_df = pd.concat([all_df, df], axis=0)
+    return all_df
+
+
 if __name__ == '__main__':
-    body = create_body([622801209626283, 2251496734604682, 3262727548303974])
-    request = post_request(body)
-    print(request)
+    data = fetch_data(
+        ids=[622801209626283, 3262727548303974],
+        start_date='2w-ago',
+        end_date='now',
+        granularity='5m',
+        limit=5000,
+        aggregates=['average']
+    )
+    print(data)
